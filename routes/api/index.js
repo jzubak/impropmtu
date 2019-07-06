dc7721f578306951f2d2bc9d9f4dde6fbdd7f1ea
Costa <keith@Keiths-MacBook-Pro.local>
2019-07-06 14:16:49 -0400

    amazing

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,4 +4,11 @@ const tripRoutes = require("./trips");
 // Trip routes
 router.use("/trips", tripRoutes);
 
+// // api/search
+// router.route('/search', function(req, res) {
+//     console.log('in search route');
+
+//     //do more stuff here
+// })
+
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,17 @@ const apiRoutes = require("./api");
 // API Routes
 router.use("/api", apiRoutes);
 
+// api/search
+router.route('/api/search').post(function(req, res) {
+    console.log('in search route');
+    console.log(req.body)
+    //do more stuff here
+})
+
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
+
 
 module.exports = router;
