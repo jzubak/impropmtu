8968412388b31872eeb5220e5805956c12fa0283
bbarikbin38 <bijan.barikbin38@gmail.com>
2019-07-08 21:22:49 -0400

    db seeds updates

--- seeds/destinationsDB.js
+++ seeds/destinationsDB.js
@@ -1,158 +1,160 @@
-INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Philadelphia, PA", 0, 1, 0, 1, 1, 1, 0, 0, 1, 1);
+use impromptu;
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Lancaster, PA", 0, 0, 0, 0, 0, 1 ,0, 1, 0, 1);
+VALUES ("Philadelphia, PA", 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, "https://res.cloudinary.com/nikza/image/upload/v1562628884/Philadelphia_PA_yowvgm.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("New Hope, PA", 0, 0, 0, 1, 0, 0, 0, 1, 0, 1);
+VALUES ("Lancaster, PA", 0, 0, 0, 0, 0, 1 ,0, 1, 0, 1, "https://res.cloudinary.com/nikza/image/upload/v1562628884/Lancaster_PA_kut96m.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Atlantic City, NJ", 1, 0, 0, 1, 1, 0, 0, 0, 0, 1);
+VALUES ("New Hope, PA", 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, "https://res.cloudinary.com/nikza/image/upload/v1562629329/NewHope_PA_ohfiij.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Cape May, NJ", 1, 0, 0, 0, 0, 1, 0, 1, 1, 1);
+VALUES ("Atlantic City, NJ", 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, "https://res.cloudinary.com/nikza/image/upload/v1562628884/AtlanticCity_hxzqx9.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Atlanta, GA", 0, 1, 0, 1, 1, 1, 0, 0, 1, 2);
+VALUES ("Cape May, NJ", 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, "https://res.cloudinary.com/nikza/image/upload/v1562628884/CapeMay_NJ_s5qyru.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Chatanooga, TN", 0, 0, 1, 1, 0, 1, 0, 1, 1, 2);
+VALUES ("Atlanta, GA", 0, 1, 0, 1, 1, 1, 0, 0, 1, 2, "https://res.cloudinary.com/nikza/image/upload/v1562629231/Atlanta_zh0cxb.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Geenville, SC", 0, 1, 1, 1, 0, 0, 0, 1, 1, 2);
+VALUES ("Chatanooga, TN", 0, 0, 1, 1, 0, 1, 0, 1, 1, 2, "https://res.cloudinary.com/nikza/image/upload/v1562629231/Chattanooga_ls148h.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Athens, GA", 0, 0, 0, 1, 1, 1, 0, 0, 0, 2);
+VALUES ("Greenville, SC", 0, 1, 1, 1, 0, 0, 0, 1, 1, 2, "https://res.cloudinary.com/nikza/image/upload/v1562629231/Greenville_y6fezj.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Los Angeles, CA", 1, 1, 1, 1, 1, 0, 0, 0, 1, 3);
+VALUES ("Athens, GA", 0, 0, 0, 1, 1, 1, 0, 0, 0, 2, "https://res.cloudinary.com/nikza/image/upload/v1562629231/Athens_GA_yuaewq.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Santa Barbera, CA", 0, 1, 1, 1, 0, 1, 0, 1, 1, 3);
+VALUES ("Los Angeles, CA", 1, 1, 1, 1, 1, 0, 0, 0, 1, 3, "https://res.cloudinary.com/nikza/image/upload/v1562629398/LosAngeles_u7hb3b.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Laguna Beach, CA", 1, 0, 1, 1, 0, 0, 0, 1, 1, 3);
+VALUES ("Santa Barbara, CA", 0, 1, 1, 1, 0, 1, 0, 1, 1, 3, "https://res.cloudinary.com/nikza/image/upload/v1562629398/SantaBarbara_ahhrac.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("DisneyLand Anahemeim, CA", 0, 0, 0, 0, 0, 0, 0, 0, 1, 3);
+VALUES ("Laguna Beach, CA", 1, 0, 1, 1, 0, 0, 0, 1, 1, 3, "https://res.cloudinary.com/nikza/image/upload/v1562629398/laguna.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Palm Springs, CA", 0, 0, 1, 1, 1, 1, 0, 1, 0, 3);
+VALUES ("DisneyLand Anahemeim, CA", 0, 0, 0, 0, 0, 0, 0, 0, 1, 3, "https://res.cloudinary.com/nikza/image/upload/v1562629398/Disney%20Land.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Channel Islands, CA", 1, 0, 1, 0, 0, 0, 0, 1, 0, 3);
+VALUES ("Palm Springs, CA", 0, 0, 1, 1, 1, 1, 0, 1, 0, 3, "https://res.cloudinary.com/nikza/image/upload/v1562629398/PalmSprings_CA_isnhhd.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Chicago, IL", 0, 1, 1, 1, 1, 1, 0, 0, 1, 4);
+VALUES ("Channel Islands, CA", 1, 0, 1, 0, 0, 0, 0, 1, 0, 3, "https://res.cloudinary.com/nikza/image/upload/v1562629398/ChannelIslands_CA_tcglkd.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Harbour Country, MI", 1, 0, 1, 1, 0, 0, 0, 1, 1, 4);
+VALUES ("Chicago, IL", 0, 1, 1, 1, 1, 1, 0, 0, 1, 4, "https://res.cloudinary.com/nikza/image/upload/v1562629416/Chicago_IL_ys2lvf.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Indiana Dunes, IN", 1, 0, 1, 0, 0, 0, 0, 0, 1, 4);
+VALUES ("Harbour Country, MI", 1, 0, 1, 1, 0, 0, 0, 1, 1, 4, "https://res.cloudinary.com/nikza/image/upload/v1562629416/HarborCountry_MI_clquey.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Dallas/Fort worth, TX", 0, 1, 0, 1, 1, 0, 0, 0, 0, 5);
+VALUES ("Indiana Dunes, IN", 1, 0, 1, 0, 0, 0, 0, 0, 1, 4, "https://res.cloudinary.com/nikza/image/upload/v1562629416/IndianaDunes_IN_g2qe8f.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Denver, CO", 0, 1, 1, 1, 1, 0, 1, 0, 1, 6);
+VALUES ("Dallas/Fort worth, TX", 0, 1, 0, 1, 1, 0, 0, 0, 0, 5, "https://res.cloudinary.com/nikza/image/upload/v1562629318/Dallas_dspo2k.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Boulder, CO", 0, 0, 1, 1, 1, 0, 1, 0, 1, 6);
+VALUES ("Denver, CO", 0, 1, 1, 1, 1, 0, 1, 0, 1, 6, "https://res.cloudinary.com/nikza/image/upload/v1562629307/Denver_CO_yknkni.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Vail, CO", 0, 0, 1, 0, 1, 0, 1, 0, 1, 6);
+VALUES ("Boulder, CO", 0, 0, 1, 1, 1, 0, 1, 0, 1, 6, "https://res.cloudinary.com/nikza/image/upload/v1562629307/Boulder_CO_qxyig7.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Rocky Mountain National Park, CO", 0, 0, 1, 0, 0, 0, 0, 0, 1, 6);
+VALUES ("Vail, CO", 0, 0, 1, 0, 1, 0, 1, 0, 1, 6, "https://res.cloudinary.com/nikza/image/upload/v1562629307/Vail_Co_gf4cil.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Brekenridge, CO", 0, 0, 1, 0, 0, 1, 1, 1, 1, 6);
+VALUES ("Rocky Mountain National Park, CO", 0, 0, 1, 0, 0, 0, 0, 0, 1, 6, "https://res.cloudinary.com/nikza/image/upload/v1562629307/Rocky_Mountain_National_Park_CO_uwvidu.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Fort Collins, CO", 0, 1, 1, 1, 0, 0, 0, 0, 1, 6);
+VALUES ("Brekenridge, CO", 0, 0, 1, 0, 0, 1, 1, 1, 1, 6, "https://res.cloudinary.com/nikza/image/upload/v1562629307/Brekenridge_CO_nv1b88.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("New York, NY", 0, 1, 0, 1, 1, 1, 0, 0, 1, 7);
+VALUES ("Fort Collins, CO", 0, 1, 1, 1, 0, 0, 0, 0, 1, 6, "https://res.cloudinary.com/nikza/image/upload/v1562629307/FortCollins_CO_tv4zqb.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Long Island, NY", 1, 0, 1, 1, 0, 1, 0, 0, 1, 7);
+VALUES ("New York, NY", 0, 1, 0, 1, 1, 1, 0, 0, 1, 7, "https://res.cloudinary.com/nikza/image/upload/v1562629329/NewYork_NY_tik7va.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("San Francisco, CA", 0, 1, 1, 1, 1, 1, 0, 1, 1, 8);
+VALUES ("Long Island, NY", 1, 0, 1, 1, 0, 1, 0, 0, 1, 7, "https://res.cloudinary.com/nikza/image/upload/v1562629345/LongIsland_NY_csc3rx.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Big Sur, CA", 0, 0, 1, 0, 0, 0, 0, 1, 1, 8);
+VALUES ("San Francisco, CA", 0, 1, 1, 1, 1, 1, 0, 1, 1, 8, "https://res.cloudinary.com/nikza/image/upload/v1562629461/SanFrancisco_CA_ganpzv.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Napa Valley, CA", 0, 0, 1, 1, 0, 0, 0, 1, 0, 8);
+VALUES ("Big Sur, CA", 0, 0, 1, 0, 0, 0, 0, 1, 1, 8, "https://res.cloudinary.com/nikza/image/upload/v1562629461/BigSur_CA_nnfps4.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Monterrey, CA", 1, 1, 1, 0, 0, 0, 0, 1, 1, 8);
+VALUES ("Napa Valley, CA", 0, 0, 1, 1, 0, 0, 0, 1, 0, 8, "https://res.cloudinary.com/nikza/image/upload/v1562629461/NapaValley_CA_injime.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Silicon Valley, CA", 0, 1, 0, 1, 1, 0, 0, 0, 0, 8);
+VALUES ("Monterrey, CA", 1, 1, 1, 0, 0, 0, 0, 1, 1, 8, "https://res.cloudinary.com/nikza/image/upload/v1562629461/Monterrey_CA_law6le.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Las Vegas, NV", 0, 1, 0, 1, 1, 0, 0, 0, 0, 9);
+VALUES ("Silicon Valley, CA", 0, 1, 0, 1, 1, 0, 0, 0, 0, 8, "https://res.cloudinary.com/nikza/image/upload/v1562629461/SiliconValley_CA_mtrwvc.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Death Valley, CA", 0, 0, 1, 0, 0, 0, 0, 1, 0, 9);
+VALUES ("Las Vegas, NV", 0, 1, 0, 1, 1, 0, 0, 0, 0, 9, "https://res.cloudinary.com/nikza/image/upload/v1562629377/LasVegas_NV_rzhxny.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Grand Canyon, AZ", 0, 0, 1, 0, 0, 0, 0, 1, 1, 9);
+VALUES ("Death Valley, CA", 0, 0, 1, 0, 0, 0, 0, 1, 0, 9, "https://res.cloudinary.com/nikza/image/upload/v1562629377/DeathValley_CA_qtcaph.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Seattle, WA", 0, 1, 1, 1, 1, 1, 0, 0, 1, 10);
+VALUES ("Grand Canyon, AZ", 0, 0, 1, 0, 0, 0, 0, 1, 1, 9, "https://res.cloudinary.com/nikza/image/upload/v1562629377/GrandCanyon_AZ_zmcwai.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("San Juan Islands, WA", 0, 0, 1, 0, 0, 0, 0, 1, 1, 10);
+VALUES ("Seattle, WA", 0, 1, 1, 1, 1, 1, 0, 0, 1, 10, "https://res.cloudinary.com/nikza/image/upload/v1562633549/Seattle_WA_rgn14n.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Washington Coast", 1, 0, 1, 0, 0, 0, 0, 1, 1, 10);
+VALUES ("San Juan Islands, WA", 0, 0, 1, 0, 0, 0, 0, 1, 1, 10, "https://res.cloudinary.com/nikza/image/upload/v1562633549/SanJuanIslands_WA_nocrs3.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Charlotte, NC", 0, 1, 0, 1, 1, 0, 0, 0, 0, 11);
+VALUES ("Washington Coast", 1, 0, 1, 0, 0, 0, 0, 1, 1, 10, "https://res.cloudinary.com/nikza/image/upload/v1562633549/WashingtonCoast_WA_nxcfht.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Asheville, NC", 0, 1, 1, 1, 1, 0, 0, 1, 1, 11);
+VALUES ("Charlotte, NC", 0, 1, 0, 1, 1, 0, 0, 0, 0, 11, "https://res.cloudinary.com/nikza/image/upload/v1562629295/Charlotte_NC_wxz6oq.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Greenville, SC", 0, 1, 1, 1, 0, 0, 0, 1, 1, 11);
+VALUES ("Asheville, NC", 0, 1, 1, 1, 1, 0, 0, 1, 1, 11, "https://res.cloudinary.com/nikza/image/upload/v1562629295/Asheville_NC_hr99ev.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Orlando, FL", 0, 1, 0, 1, 1, 0, 0, 0, 1, 12);
+VALUES ("Greenville, SC", 0, 1, 1, 1, 0, 0, 0, 1, 1, 11, "https://res.cloudinary.com/nikza/image/upload/v1562629231/Greenville_y6fezj.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Orlando Theme Parks, FL", 0, 0, 0, 0, 0, 0, 0, 0, 1, 12);
+VALUES ("Orlando, FL", 0, 1, 0, 1, 1, 0, 0, 0, 1, 12, "https://res.cloudinary.com/nikza/image/upload/v1562629431/Orlando_FL_mjncyu.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Cape Canaveral, FL", 1, 0, 0, 0, 0, 1, 0, 1, 1, 12);
+VALUES ("Orlando Theme Parks, FL", 0, 0, 0, 0, 0, 0, 0, 0, 1, 12, "https://res.cloudinary.com/nikza/image/upload/v1562629431/OrlandoThemeParks_fazf8h.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("St. Augustine, FL", 1, 0, 0, 0, 0, 1, 0, 1, 1, 12);
+VALUES ("Cape Canaveral, FL", 1, 0, 0, 0, 0, 1, 0, 1, 1, 12, "https://res.cloudinary.com/nikza/image/upload/v1562629431/CapeCanaveralFL_f4zzvh.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Ocala National Forest, FL", 0, 0, 1, 0, 0, 0, 0, 1, 1, 12);
+VALUES ("St. Augustine, FL", 1, 0, 0, 0, 0, 1, 0, 1, 1, 12, "https://res.cloudinary.com/nikza/image/upload/v1562629431/StAugustine_FL_bttrvm.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Phoenix, AZ", 0, 1, 0, 1, 1, 0, 0, 0, 0, 13);
+VALUES ("Ocala National Forest, FL", 0, 0, 1, 0, 0, 0, 0, 1, 1, 12, "https://res.cloudinary.com/nikza/image/upload/v1562629431/OclalaNationalForest_fi4j67.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Sedona, AZ", 0, 0, 1, 0, 0, 1, 0, 1, 1, 13);
+VALUES ("Phoenix, AZ", 0, 1, 0, 1, 1, 0, 0, 0, 0, 13, "https://res.cloudinary.com/nikza/image/upload/v1562629449/Phoenix_AZ_lbzzjh.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("New York, NY", 0, 1, 0, 1, 1, 1, 0, 0, 1, 14);
+VALUES ("Sedona, AZ", 0, 0, 1, 0, 0, 1, 0, 1, 1, 13, "https://res.cloudinary.com/nikza/image/upload/v1562629449/Sedona_AZ_t1vfjt.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Poconos, PA", 0, 0, 1, 0, 0, 0, 0, 1, 1, 14);
+VALUES ("New York, NY", 0, 1, 0, 1, 1, 1, 0, 0, 1, 14, "https://res.cloudinary.com/nikza/image/upload/v1562629345/NewYork_NY_bowvty.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Sandy Hook, NJ", 1, 0, 0, 0, 0, 0, 0, 1, 1, 14);
+VALUES ("Poconos, PA", 0, 0, 1, 0, 0, 0, 0, 1, 1, 14, "https://res.cloudinary.com/nikza/image/upload/v1562629329/Poconos_PA_ebetod.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("New Hope, PA / Lambertville, NJ", 0, 0, 0, 1, 0,0, 0, 1, 0, 14);
+VALUES ("Sandy Hook, NJ", 1, 0, 0, 0, 0, 0, 0, 1, 1, 14, "https://res.cloudinary.com/nikza/image/upload/v1562629329/SandyHook_NJ_memwdk.jpg");
 
 INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
-VALUES ("Hudson Valley and Catskills, NY", 0, 0, 1, 0, 0, 0, 1, 1, 1, 14);
+VALUES ("New Hope, PA / Lambertville, NJ", 0, 0, 0, 1, 0,0, 0, 1, 0, 14, "https://res.cloudinary.com/nikza/image/upload/v1562628884/NewHope_PA_btglg4.jpg");
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Hudson Valley and Catskills, NY", 0, 0, 1, 0, 0, 0, 1, 1, 1, 14, "https://res.cloudinary.com/nikza/image/upload/v1562634435/Catskills_NY_xg4s4d.jpg");
