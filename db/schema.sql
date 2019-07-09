441894cf79158b65bd4f7b3a6b8a61ec87b51702
bbarikbin38 <bijan.barikbin38@gmail.com>
2019-07-08 21:22:19 -0400

    db updates

--- db/schema.sql
+++ db/schema.sql
@@ -1,25 +1,25 @@
 -- Drop Database impromptu;
 CREATE DATABASE impromptu;
 
-CREATE TABLE airports (
+  CREATE TABLE airports (
 	airportID INT NOT NULL AUTO_INCREMENT,
     code VARCHAR (255) NOT NULL,
-	beach VARCHAR(11) NOT NULL,
-    urban VARCHAR(11) NOT NULL,
-    hiking VARCHAR(11) NOT NULL,
-    food VARCHAR(11) NOT NULL,
-    nightlife VARCHAR(11) NOT NULL,
-    historic VARCHAR(11) NOT NULL,
-    ski VARCHAR(11) NOT NULL,
-    quiet VARCHAR(11) NOT NULL,
-    kidfriendly VARCHAR(11) NOT NULL, 
+	beach VARCHAR (255) NOT NULL,
+    urban VARCHAR (255) NOT NULL,
+    hiking VARCHAR (255) NOT NULL,
+    food VARCHAR (255) NOT NULL,
+    nightlife VARCHAR (255) NOT NULL,
+    historic VARCHAR (255) NOT NULL,
+    ski VARCHAR (255) NOT NULL,
+    quiet VARCHAR (255) NOT NULL,
+    kidfriendly VARCHAR (255) NOT NULL, 
     PRIMARY KEY (airportID)
   );
 
 CREATE TABLE destinations (
 	destinationsID INT NOT NULL AUTO_INCREMENT,
     cityName VARCHAR (255) NOT NULL,
-	beach VARCHAR(11) NOT NULL,
+	  beach VARCHAR(11) NOT NULL,
     urban VARCHAR(11) NOT NULL,
     hiking VARCHAR(11) NOT NULL,
     food VARCHAR(11) NOT NULL,
@@ -30,6 +30,6 @@ CREATE TABLE destinations (
     kidfriendly VARCHAR(11) NOT NULL,
     PRIMARY KEY (destinationsID),
     airportID INT NOT NULL,
-	FOREIGN KEY fk_airport(airportID)
+	  FOREIGN KEY fk_airport(airportID)
     REFERENCES airports(airportID)
   );
