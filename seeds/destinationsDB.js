327287a7ed0e3c21af190f87ab5c11553b99c863
bbarikbin38 <bijan.barikbin38@gmail.com>
2019-07-08 20:54:07 -0400

    destination seeds

---
+++ seeds/destinationsDB.js
@@ -0,0 +1,158 @@
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Philadelphia, PA", 0, 1, 0, 1, 1, 1, 0, 0, 1, 1);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Lancaster, PA", 0, 0, 0, 0, 0, 1 ,0, 1, 0, 1);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("New Hope, PA", 0, 0, 0, 1, 0, 0, 0, 1, 0, 1);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Atlantic City, NJ", 1, 0, 0, 1, 1, 0, 0, 0, 0, 1);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Cape May, NJ", 1, 0, 0, 0, 0, 1, 0, 1, 1, 1);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Atlanta, GA", 0, 1, 0, 1, 1, 1, 0, 0, 1, 2);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Chatanooga, TN", 0, 0, 1, 1, 0, 1, 0, 1, 1, 2);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Geenville, SC", 0, 1, 1, 1, 0, 0, 0, 1, 1, 2);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Athens, GA", 0, 0, 0, 1, 1, 1, 0, 0, 0, 2);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Los Angeles, CA", 1, 1, 1, 1, 1, 0, 0, 0, 1, 3);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Santa Barbera, CA", 0, 1, 1, 1, 0, 1, 0, 1, 1, 3);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Laguna Beach, CA", 1, 0, 1, 1, 0, 0, 0, 1, 1, 3);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("DisneyLand Anahemeim, CA", 0, 0, 0, 0, 0, 0, 0, 0, 1, 3);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Palm Springs, CA", 0, 0, 1, 1, 1, 1, 0, 1, 0, 3);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Channel Islands, CA", 1, 0, 1, 0, 0, 0, 0, 1, 0, 3);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Chicago, IL", 0, 1, 1, 1, 1, 1, 0, 0, 1, 4);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Harbour Country, MI", 1, 0, 1, 1, 0, 0, 0, 1, 1, 4);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Indiana Dunes, IN", 1, 0, 1, 0, 0, 0, 0, 0, 1, 4);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Dallas/Fort worth, TX", 0, 1, 0, 1, 1, 0, 0, 0, 0, 5);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Denver, CO", 0, 1, 1, 1, 1, 0, 1, 0, 1, 6);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Boulder, CO", 0, 0, 1, 1, 1, 0, 1, 0, 1, 6);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Vail, CO", 0, 0, 1, 0, 1, 0, 1, 0, 1, 6);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Rocky Mountain National Park, CO", 0, 0, 1, 0, 0, 0, 0, 0, 1, 6);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Brekenridge, CO", 0, 0, 1, 0, 0, 1, 1, 1, 1, 6);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Fort Collins, CO", 0, 1, 1, 1, 0, 0, 0, 0, 1, 6);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("New York, NY", 0, 1, 0, 1, 1, 1, 0, 0, 1, 7);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Long Island, NY", 1, 0, 1, 1, 0, 1, 0, 0, 1, 7);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("San Francisco, CA", 0, 1, 1, 1, 1, 1, 0, 1, 1, 8);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Big Sur, CA", 0, 0, 1, 0, 0, 0, 0, 1, 1, 8);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Napa Valley, CA", 0, 0, 1, 1, 0, 0, 0, 1, 0, 8);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Monterrey, CA", 1, 1, 1, 0, 0, 0, 0, 1, 1, 8);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Silicon Valley, CA", 0, 1, 0, 1, 1, 0, 0, 0, 0, 8);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Las Vegas, NV", 0, 1, 0, 1, 1, 0, 0, 0, 0, 9);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Death Valley, CA", 0, 0, 1, 0, 0, 0, 0, 1, 0, 9);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Grand Canyon, AZ", 0, 0, 1, 0, 0, 0, 0, 1, 1, 9);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Seattle, WA", 0, 1, 1, 1, 1, 1, 0, 0, 1, 10);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("San Juan Islands, WA", 0, 0, 1, 0, 0, 0, 0, 1, 1, 10);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Washington Coast", 1, 0, 1, 0, 0, 0, 0, 1, 1, 10);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Charlotte, NC", 0, 1, 0, 1, 1, 0, 0, 0, 0, 11);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Asheville, NC", 0, 1, 1, 1, 1, 0, 0, 1, 1, 11);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Greenville, SC", 0, 1, 1, 1, 0, 0, 0, 1, 1, 11);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Orlando, FL", 0, 1, 0, 1, 1, 0, 0, 0, 1, 12);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Orlando Theme Parks, FL", 0, 0, 0, 0, 0, 0, 0, 0, 1, 12);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Cape Canaveral, FL", 1, 0, 0, 0, 0, 1, 0, 1, 1, 12);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("St. Augustine, FL", 1, 0, 0, 0, 0, 1, 0, 1, 1, 12);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Ocala National Forest, FL", 0, 0, 1, 0, 0, 0, 0, 1, 1, 12);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Phoenix, AZ", 0, 1, 0, 1, 1, 0, 0, 0, 0, 13);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Sedona, AZ", 0, 0, 1, 0, 0, 1, 0, 1, 1, 13);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("New York, NY", 0, 1, 0, 1, 1, 1, 0, 0, 1, 14);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Poconos, PA", 0, 0, 1, 0, 0, 0, 0, 1, 1, 14);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Sandy Hook, NJ", 1, 0, 0, 0, 0, 0, 0, 1, 1, 14);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("New Hope, PA / Lambertville, NJ", 0, 0, 0, 1, 0,0, 0, 1, 0, 14);
+
+INSERT INTO destinations (cityname, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly, airportID, url)
+VALUES ("Hudson Valley and Catskills, NY", 0, 0, 1, 0, 0, 0, 1, 1, 1, 14);
