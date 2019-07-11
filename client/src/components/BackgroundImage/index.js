ab627438e0295a4e2b6909e1bdef6d2568989876
Costa <keith@Keiths-MacBook-Pro.local>
2019-07-10 21:02:11 -0400

    ad search style changes

---
+++ client/src/components/BackgroundImage/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function BackgroundImage(props){
+    var divStyle = {
+        backgroundImage: 'url(' + props.src + ')',
+        overflow: "hidden",
+        position: "fixed",
+        top: 0,
+        width: "100%",
+        height: "100%",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "50% 50%" ,
+        zIndex: -10
+      };
+
+    return(
+        <div style={divStyle}></div>
+    )
+}
+
+export default BackgroundImage
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Logo from "../../images/logo(horizontal).png"
+import Icon from "../../images/icon.png"
+import "./style.css"
 
 function Nav() {
   return (
     <nav className="navbar">
       <a className="" href="/">
-        Impromptu
+        <img src={Logo} alt="logo" className="height mt-1 full"></img>
+        <img src={Icon} alt="logo" className="height mt-1 icon"></img>
       </a>
     </nav>
   );
