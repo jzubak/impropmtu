b0ec6f6ae91dbdefb142e5b6dbf3a36fcdf755a7
jzubak <jzubak81@gmail.com>
2019-07-10 20:18:07 -0400

    queryies almost done

--- db/seeds/airportsTable.sql
+++ db/seeds/airportsTable.sql
@@ -1,42 +1,29 @@
-INSERT INTO airports (code, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly)
-VALUES ("PHL", 1, 1, 0, 1, 1, 1, 0, 1, 1);
 
-INSERT INTO airports (code, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly)
-VALUES ("ATL", 0, 1, 1, 1, 1,1, 0, 1, 1);
-use impromptu; 
-
-INSERT INTO airports (code, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly)
-VALUES ("LAX", 1, 1, 1, 1, 1, 1, 0, 1, 1);
-
-INSERT INTO airports (code, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly)
-VALUES ("ORD", 1, 1, 1, 1, 1, 1, 0, 1, 1);
-
-INSERT INTO airports (code, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly)
-VALUES ("DFW", 0, 1, 0, 1, 1, 0, 0, 0, 0);
-
-INSERT INTO airports (code, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly)
-VALUES ("DEN", 0, 1, 1, 1, 1, 1, 1, 1, 1);
-
-INSERT INTO airports (code, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly)
-VALUES ("JFK", 1, 1, 1, 1, 1, 1, 0, 1, 1);
-
-INSERT INTO airports (code, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly)
-VALUES ("SFO", 1, 1, 1, 1, 1, 1, 0, 1, 1);
-
-INSERT INTO airports (code, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly)
-VALUES ("LAS", 0, 1, 1, 0, 1, 0 , 0, 1, 1);
-
-INSERT INTO airports (code, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly)
-VALUES ("SEA", 1, 1, 1, 1, 1, 1, 0, 1, 1);
-
-INSERT INTO airports (code, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly)
-VALUES ("CLT", 0, 1, 1, 1, 1, 0, 0, 1, 1);
-
-INSERT INTO airports (code, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly)
-VALUES ("MCO", 1, 1, 1, 1, 1, 1, 0 ,1, 1);
-
-INSERT INTO airports (code, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly)
-VALUES ("PHX", 0, 1, 1, 1, 1, 1, 0, 1, 1);
-
-INSERT INTO airports (code, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly)
-VALUES ("EWR", 1, 1, 1, 1, 1, 1, 1, 1, 1);
+INSERT INTO airports (airportID, airportairportCode, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly)
+VALUES (1, "PHL", 1, 1, 0, 1, 1, 1, 0, 1, 1);
+INSERT INTO airports (airportID, airportCode, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly)
+VALUES (2, "ATL", 0, 1, 1, 1, 1,1, 0, 1, 1);
+INSERT INTO airports (airportID, airportCode, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly)
+VALUES (3, "LAX", 1, 1, 1, 1, 1, 1, 0, 1, 1);
+INSERT INTO airports (airportID, airportCode, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly)
+VALUES (4, "ORD", 1, 1, 1, 1, 1, 1, 0, 1, 1);
+INSERT INTO airports (airportID, airportCode, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly)
+VALUES (5, "DFW", 0, 1, 0, 1, 1, 0, 0, 0, 0);
+INSERT INTO airports (airportID, airportCode, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly)
+VALUES (6, "DEN", 0, 1, 1, 1, 1, 1, 1, 1, 1);
+INSERT INTO airports (airportID, airportCode, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly)
+VALUES (7, "JFK", 1, 1, 1, 1, 1, 1, 0, 1, 1);
+INSERT INTO airports (airportID, airportCode, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly)
+VALUES (8, "SFO", 1, 1, 1, 1, 1, 1, 0, 1, 1);
+INSERT INTO airports (airportID, airportCode, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly)
+VALUES (9, "LAS", 0, 1, 1, 0, 1, 0 , 0, 1, 1);
+INSERT INTO airports (airportID, airportCode, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly)
+VALUES (10, "SEA", 1, 1, 1, 1, 1, 1, 0, 1, 1);
+INSERT INTO airports (airportID, airportCode, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly)
+VALUES (11, "CLT", 0, 1, 1, 1, 1, 0, 0, 1, 1);
+INSERT INTO airports (airportID, airportCode, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly)
+VALUES (12, "MCO", 1, 1, 1, 1, 1, 1, 0 ,1, 1);
+INSERT INTO airports (airportID, airportCode, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly)
+VALUES (13, "PHX", 0, 1, 1, 1, 1, 1, 0, 1, 1);
+INSERT INTO airports (airportID, airportCode, beach, urban, hiking, food, nightlife, historic, ski, quiet, kidfriendly)
+VALUES (14, "EWR", 1, 1, 1, 1, 1, 1, 1, 1, 1);
--- routes/index.js
+++ routes/index.js
@@ -6,61 +6,79 @@ const apiRoutes = require("./api");
 const db = require("../models");
 const Op = Sequalize.Op;
 
+let forAPI = {
+  flightAPI:[],
+  hotelsAPI:[],
+  };
 // API Routes
 router.use("/api", apiRoutes);
 
 // api/search
 //this is where we need to write the sequalize version of this SQL query:
-    //select * from airports where *column name* = true (for however many columns the input answer is not 0)
-    //this is will give us all aiport options
-    //select distinct from desinations using the foreign key from the airports query where *column name* = true (limited to the columns where the input is not 0)
-router.route('/api/search').post(function(req, res) {
-    console.log('in search route');
-    console.log(req.body)
-    db.airport.findAll({where: {[Op.or]: 
-          [{beach: req.body.Beach},
-          {urban: req.body.Urban},
-          {hiking: req.body.Hiking},
-          {food: req.body.Food},
-          {nightlife: req.body.Nightlife},
-          {historic: req.body.Historic},
-          {ski: req.body.Music},
-          {quiet: req.body.Quiet},
-          {kidfriendly: req.body.KidFriendly}]}
-        }).then(function(dbFilter){
-            console.log("out of the databse query");
-            console.log("---------------------------", req.body);
-            console.log(dbFilter);
-            var initialres = JSON.stringify(dbFilter);
-            console.log("this is the inital results")
-            console.log(initialres)
-            console.log("in the 2nd call")
-
-          db.destination.findAll({
-            where: {
-            beach: req.body.Beach,
-            urban: req.body.Urban,
-            hiking: req.body.Hiking,
-            food: req.body.Food,
-            nightlife: req.body.Nightlife,
-            historic: req.body.Historic,
-            ski: req.body.Music,
-            quiet: req.body.Quiet,
-            kidfriendly: req.body.KidFriendly,
-          }
-          })}).then(function(destinationFilter){
-            console.log("filtered through destination table")
-            console.log(destinationFilter)
-          }).catch(function(err){
+//select * from airports where *column name* = true (for however many columns the input answer is not 0)
+//this is will give us all aiport options
+//select distinct from desinations using the foreign key from the airports query where *column name* = true (limited to the columns where the input is not 0)
+router.route('/api/search').post(function (req, res) {
+  console.log('in search route');
+  console.log(req.body)
+  db.airport.findAll({
+    where: {
+      [Op.or]:
+        [{ beach: req.body.Beach },
+        { urban: req.body.Urban },
+        { hiking: req.body.Hiking },
+        { food: req.body.Food },
+        { nightlife: req.body.Nightlife },
+        { historic: req.body.Historic },
+        { ski: req.body.Music },
+        { quiet: req.body.Quiet },
+        { kidfriendly: req.body.KidFriendly }]
+    }
+  }).then(function (dbFilter) {
+    // console.log("out of the databse query");
+    // console.log("---------------------------", req.body);
+    // console.log(dbFilter);
+    var initialres = JSON.stringify(dbFilter);
+    // console.log("this is the inital results")
+    console.log(initialres)
+    // console.log("in the 2nd call")
+    // console.log("-req.body again-", req.body);
+    db.destination.findAll({
+      where: {
+        beach: req.body.Beach,
+        urban: req.body.Urban,
+        hiking: req.body.Hiking,
+        food: req.body.Food,
+        nightlife: req.body.Nightlife,
+        historic: req.body.Historic,
+        ski: req.body.Music,
+        quiet: req.body.Quiet,
+        kidfriendly: req.body.KidFriendly,
+      }
+    }).then(function (destinationFilter) {
+      console.log("filtered through destination table")
+      // console.log(destinationFilter)
+      destFiltered = JSON.stringify(destinationFilter);
+      // console.log(destFiltered);
+      // console.log(initialres);
+    }).then(function(endQ){
+      console.log("this is where the final obejct of arrays will be");
+      console.log("this is the results of the airport query");
+      console.log(initialres);
+      console.log("this is the results of the destinations query");
+      console.log(destFiltered);
+    })
+    .catch(function (err) {
       console.log(err);
       res.json(err);
+    })
   })
 });
 
 
 
 // If no API routes are hit, send the React app
-router.use(function(req, res) {
+router.use(function (req, res) {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
